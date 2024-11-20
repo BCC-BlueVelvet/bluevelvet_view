88c5377d8862c85e382fb84415f42ea920851a1e
gustavoborinn <gustavo.nascimento@sou.unifal-mg.edu.br>
2024-11-19 21:00:33 -0300

    feat: Add backend integration

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "bootstrap/dist/css/bootstrap.min.css"; // Importa o CSS do Bootstrap par
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"; // Importa componentes do react-router-dom para navegação
 import UpdateProduct from "./pages/UpdateProduct"; // Importa a página de atualização de produto
 import FindProducts from "./pages/FindProducts"; // Importa a página de busca de produtos
+import AddProduct from "./pages/AddProduct"; // Importa o novo componente de adição de produtos
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
 
         {/* Rota para o componente de atualização de produto com ID como parâmetro */}
         <Route path="/update-product/:id" element={<UpdateProduct />} />
+        <Route path="/add-product" element={<AddProduct />} /> {/* Nova rota */}
       </Routes>
     </Router>
   );
